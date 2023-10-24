cd93a2f3503670911afd5fda7a8edf831c80fa2b
SigureMo <sigure.qaq@gmail.com>
2023-10-25 02:56:00 +0800

    chore: update locale

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -3,17 +3,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Hi, PFCCLab!',
   description: 'PFCC 社区博客～',
+  lang: 'zh-CN',
   cleanUrls: true,
   head: [
-    ['meta', { name: 'twitter:site', content: '@vuejs' }],
-    ['meta', { name: 'twitter:card', content: 'summary' }],
-    [
-      'meta',
-      {
-        name: 'twitter:image',
-        content: 'https://vuejs.org/images/logo.png',
-      },
-    ],
     [
       'link',
       {
