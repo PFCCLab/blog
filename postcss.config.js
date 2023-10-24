a4c9bc556f4bb7d8c016cee5e5eef210e5d75942
SigureMo <sigure.qaq@gmail.com>
2023-10-25 02:14:10 +0800

    refactor: move code to `src/`

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ import tailwindTypography from '@tailwindcss/typography'
 export default {
   plugins: [
     tailwind({
-      content: ['./.vitepress/theme/**/*.vue'],
+      content: ['./src/.vitepress/theme/**/*.vue'],
       plugins: [tailwindTypography],
     }),
   ],
