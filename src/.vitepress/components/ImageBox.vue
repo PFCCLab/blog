8e1c73793aa00110eda603986ecd711768294e2e
gouzi <530971494@qq.com>
2023-11-29 01:29:26 +0800

    fix codestyle

--- src/.vitepress/components/ImageBox.vue
+++ src/.vitepress/components/ImageBox.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-
-import {computed} from "vue";
+import { computed } from 'vue'
 
 const { url, describe, centerStyle, legendStyle } = defineProps<{
   url: string
@@ -13,21 +12,24 @@ const src = computed(() => {
   return url
 })
 
-const alt = computed(() =>{
+const alt = computed(() => {
   return describe
 })
 
-const centerstyle = computed(() =>{
+const centerstyle = computed(() => {
   const centerStyle_ = centerStyle ?? null
-  if (centerStyle_ == null){
+  if (centerStyle_ == null) {
     return 'text-align: center'
   } else {
     return centerStyle + 'text-align: center'
   }
 })
 
-const legendstyles = computed(() =>{
-  return legendStyle ?? "color: orange;border-bottom: 1px solid #d9d9d9;display: inline-block;color: #999;padding: 2px;"
+const legendstyles = computed(() => {
+  return (
+    legendStyle ??
+    'color: orange;border-bottom: 1px solid #d9d9d9;display: inline-block;color: #999;padding: 2px;'
+  )
 })
 </script>
 
