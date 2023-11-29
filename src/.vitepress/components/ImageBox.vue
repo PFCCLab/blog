98db1000339540c965ad9cc43315fafc6004ed93
gouzi <530971494@qq.com>
2023-11-29 08:08:23 +0800

    rm null

--- src/.vitepress/components/ImageBox.vue
+++ src/.vitepress/components/ImageBox.vue
@@ -3,7 +3,7 @@ import { computed } from 'vue'
 
 const { url, description, centerStyle, legendStyle } = defineProps<{
   url: string
-  description?: string
+  description: string
   centerStyle?: string
   legendStyle?: string
 }>()
